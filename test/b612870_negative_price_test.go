b4625b866301b9786faaabd713ad9b79b15231e6
Unsull <kiadtisakpattanee@gmail.com>
2026-01-07 11:10:25 +0700

    add some

---
+++ test/b612870_negative_price_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"LabTest/model"
+
+	"testing"
+
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+)
+
+func ValidateNegativePrice(t *testing.T) {
+	g := NewGomWiht(t)
+
+	g.Run("Price must be between 50 and 5000", func(t *testing.T) {
+		book := model.Books{
+			Title: "text",
+			Price: 5,
+			Code:  "B6612870",
+		}
+
+		ok, err := govalidator.ValidateStruct(book)
+		g.Expect(ok).To(BeFalse)
+		g.Expect(err).ToNot(BeNil)
+		g.Expect(err.Error()).To(Equal("Price must be between 50 and 5000"))
+	})
+
+	g.Run()
+}
